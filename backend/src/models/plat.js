7724ffb0b080ad64dc7a13a45b8c40f4b949dd17
chaimaefahi39-source <chaimaefahi39@gmail.com>
2026-07-23 14:23:28 +0200

    complete task1- database models and server initialization

---
+++ backend/src/models/plat.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Plat = sequelize.define('Plat', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  nom: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  prix: {
+    type: DataTypes.DECIMAL(6, 2),
+    allowNull: false,
+  },
+  categorie: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  disponible: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  }
+}, {
+  tableName: 'plats',
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: false
+});
+
+module.exports = Plat;
