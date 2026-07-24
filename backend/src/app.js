9a6fb1ef3023708cf3da7312a37a523d548b830f
chaimaefahi39-source <chaimaefahi39@gmail.com>
2026-07-24 11:16:12 +0200

    complete task3 Integrate Scalar UI API documentation using OpenAPI

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const sequelize = require('./config/database');
 const platRoutes = require('./routes/platRoutes');
-const commandeRoutes = require('./routes/commandeRoutes'); 
-const { Plat, Commande } = require('./models/index'); 
-require('dotenv').config();
-
+const commandeRoutes = require('./routes/commandeRoutes');
+const { apiReference } = require('@scalar/express-api-reference'); 
+const openapiDoc = require('./docs/openapi.json'); 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.use('/api/plats', platRoutes);
-app.use('/api/commandes', commandeRoutes); 
+app.use('/api/commandes', commandeRoutes);
+app.use(
+  '/reference',
+  apiReference({
+    spec: {
+      content: openapiDoc,
+    },
+  })
+);
 
 sequelize.sync({ force: false }).then(() => {
   console.log('Connexion a PostgreSQL réussie & Relations synchronisées !');
